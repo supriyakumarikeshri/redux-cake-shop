70af645938f52e1b4f19a6d1f4c36e9cfce51e21
supriyakumarikeshri <supriyakumari2393@gmail.com>
2020-05-18 23:38:56 +0530

    Updates

---
+++ precache-manifest.21293d954362c35994a99db33070ace9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f406edc5f156cd0b966e11876f993b80",
+    "url": "/redux-cake-shop/index.html"
+  },
+  {
+    "revision": "56b4f725b5cb55948c5f",
+    "url": "/redux-cake-shop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c11a83dc0a2176c7e512",
+    "url": "/redux-cake-shop/static/js/2.80b58d39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-cake-shop/static/js/2.80b58d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b4f725b5cb55948c5f",
+    "url": "/redux-cake-shop/static/js/main.32e89f7e.chunk.js"
+  },
+  {
+    "revision": "c6963f115957d5e8ed0c",
+    "url": "/redux-cake-shop/static/js/runtime-main.640e5575.js"
+  }
+]);
